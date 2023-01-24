ec39975a9d0a752a445179e85a51b345255f4489
mrusman01 <mianusmanjaved49@gmail.com>
2023-01-25 00:06:17 +0500

    nothing

--- main.js
+++ main.js
@@ -8,3 +8,8 @@ function changeIcon(event) {
   ham.children[2].classList.toggle("rotate-right");
   document.querySelector(".drawer-items").classList.toggle("visible");
 }
+
+gsap.from(".nav-main", { duration: 2, y: "-100%", ease: "bounce" });
+gsap.from(".hero-img", { duration: 2, x: "90%", ease: "bounce" });
+gsap.from(".heroSection_text", { duration: 2, y: "170%", ease: "bounce" });
+// gsap.to(".hero-img", { rotation: 360, duration: 2.8, ease: "power10.in" });
